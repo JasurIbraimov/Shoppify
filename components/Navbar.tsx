1cc405e6b4127f457989a027f10794728965bfa7
Jasur <ibraimov0409@mail.ru>
2023-08-15 09:45:16 +0600

    Started a new project

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NavLinks } from "@/constants";
+import AuthProviders from "./AuthProviders";
+
+const Navbar = () => {
+    const session = false;
+    return (
+        <nav className="app-nav">
+            <div className="container gap-10 flex justify-between items-center">
+                <Link className="logo" href="/">
+                    <Image
+                        src="/logo.svg"
+                        alt="Shoppify Logo"
+                        width={40}
+                        height={40}
+                    />
+                    <p>Shoppify</p>
+                </Link>
+
+                <ul className="xl:flex hidden text-base gap-5">
+                    {NavLinks.map((link) => (
+                        <Link className="link" href={link.href} key={link.key}>
+                            <span>{link.text}</span>
+                        </Link>
+                    ))}
+                </ul>
+
+                <div className="gap-4">
+                        {
+                            session ? (<>
+                                UserImage 
+                                <Link href="/add-product">
+                                    Add product
+                                </Link>
+                            </>) : (
+                                <AuthProviders />
+                            )
+                        }
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary": "#00C9A7",
+        "secondary": "#4B31A1",
+        "light": "#EFFCF8",
+        "dark": "#545479",
+        "danger": "#FF9B82",
+        "warning": "#ED9660",
+        "success": "#CDEE8F"
+      },
+      maxWidth: {
+        "10xl": "1680px"
       },
     },
   },
   plugins: [],
+  "darkMode": "class"
 }
 export default config
